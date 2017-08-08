82105c63c02f56627db41fe35f6ee367576570c4
s3286780 <30459762+s3286780@users.noreply.github.com>
2017-08-08 21:23:20 +1000

    Update index.blade.php
    href links need to be inputted once pages created

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,6 +4,15 @@
     $page_title="Welcome";
 ?>
 
-<h1>Home page</h1>
+<div class='jumbotron text-center'>
+    <h1>Where's My Tradie!</h1>
+    <p>Welcome to Where's My Tradie</p>
+    <p>Please Login, Register, or Advertise your business.</p>
+    <p>
+        <a class = 'btn btn-primary btn-lg' href = '/login' role = 'button'   >Login</a>
+        <a class = 'btn btn-success btn-lg' href = '/register' role = 'button'>Register</a>
+        <a class = 'btn btn-warning btn-lg' href = '/register' role = 'button'>Advertise</a>
+    </p> 
+</div>
 
 @stop
