2dddee56f7eb1902e5d79e8f0b03f5f73fd7a0e6
s3286780 <30459762+s3286780@users.noreply.github.com>
2017-08-21 12:51:13 +1000

    ad index page created

--- app/Http/Controllers/AdvertisementsController.php
+++ app/Http/Controllers/AdvertisementsController.php
@@ -16,7 +16,7 @@ class AdvertisementsController extends Controller
      */
     public function index()
     {
-        //
+        return view('advertisements.index');
     }
 
     /**
---
+++ resources/views/advertisements/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+@section('body')
+<?php
+    $page_title="Ads";
+?>
+
+<h1>ads</h1>
+
+@endsection
