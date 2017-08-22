05e15dd9f1ded95ed4bec2905d07161787eb3d58
s3286780 <30459762+s3286780@users.noreply.github.com>
2017-08-22 13:51:13 +1000

    Registration page elements updated
    - account type added
    - warning if missing element added
    - description of account types added
    - elements made to be required

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,28 +9,66 @@
 <form method="POST" action="/auth/register">
     {!! csrf_field() !!}
 
+    <!-- Name -->
     <div>
         Name
-        <input type="text" name="name" value="{{ old('name') }}">
+        <input id='name' type="text" name="name" value="{{ old('name') }}" required autofocus>
+        @if ($errors->has('name'))
+            <span class="help-block">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+        @endif
     </div>
 
+    <!-- Email -->
     <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+        Email: 
+        <input id='email' type="email" name="email" value="{{ old('email') }}" required>
+        @if ($errors->has('email'))
+            <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+        @endif
     </div>
 
+    <!-- Password -->
     <div>
-        Password
-        <input type="password" name="password">
+        Password: 
+        <input id='password' type="password" name="password" required>
+        @if ($errors->has('password'))
+            <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+        @endif
     </div>
 
+    <!-- Confirm Password -->
     <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
+        Confirm Password: 
+        <input type="password" name="password_confirmation" required>
+    </div>
+    <!-- Account type -->
+    <div>
+        Account Type: 
+        <input type="radio" name='account_type' value='personal' >Personal
+        <input type="radio" name='account_type' value='business' >Business
+        
+        @if ($errors->has('account_type'))
+            <span class="help-block">
+                <strong>{{ $errors->first('account_type') }}</strong>
+            </span>
+        @endif
+        <div>
+            <small>Personal Account: Ables the user to search for tradies.</small>
+        </div>
+        <div>
+            <small>Business Account: For business use only, enables the user to create and post ads.</small>
+        </div>
     </div>
 
+    <!-- Submit Button -->
     <div>
-        <button type="submit">Register</button>
+        <button type="submit" class='btn btn-success'>Register</button>
     </div>
 </form>
 
