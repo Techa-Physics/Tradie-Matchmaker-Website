bb8f9092e816a95cbde2fdbcaac0115a4a26148e
Thang Truong <30461254+thanggtruongg@users.noreply.github.com>
2017-09-15 18:48:29 +1000

    css change homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,17 +5,23 @@
 ?>
 
 <div class='jumbotron text-center'>
-    <h1>Where's My Tradie!</h1>
-    <p>Welcome to Where's My Tradie</p>
-    @if(Auth::check())
+	<div class='textathome'>
+		<h1>Where's My Tradie!</h1>
+		<p>Welcome to Where's My Tradie</p>
+		@if(Auth::check())
 
-    @else
-    <p>Please Login or Regsiter.</p>
-    <p>
-        <a class = 'btn btn-primary btn-lg' href = '/auth/login' role = 'button'   >Login</a>
-        <a class = 'btn btn-success btn-lg' href = '/auth/register' role = 'button'>Register</a>
-    </p> 
-    @endif
+		@else
+		<p>Please Login or Regsiter.</p>
+		<p>
+			<a class = 'btn btn-primary btn-lg' href = '/auth/login' role = 'button'   >Login</a>
+			<a class = 'btn btn-success btn-lg' href = '/auth/register' role = 'button'>Register</a>
+		</p> 
+		@endif
+	</div>
+	<div class='pictureathome'>
+		<img src="{{ URL::to('pictures/toolman.png') }}" width="400" height="400">
+	</div>
 </div>
+
 
 @endsection
