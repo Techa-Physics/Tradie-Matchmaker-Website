136862698aaceed277ae19269e0c5b5189a03ff3
Thang Truong <30461254+thanggtruongg@users.noreply.github.com>
2017-09-25 15:13:16 +1000

    k

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,3 +2,4 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
