fb4a140b7a17cadd7adf0c010dc0e9ce712b0bbc
TechaJat <30589973+TechaJat@users.noreply.github.com>
2017-09-28 17:44:16 +1000

    Palette now affects website
    - After selecting a palette, the website will permanently switch to the palette

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,21 @@
+<?php
+    use App\Palette;
+
+    $curPalette = Palette::find(1)->currentpalette;
+    $paletteDark = "palette-". $curPalette ."-dark";
+    $paletteMedium = "palette-". $curPalette ."-medium";
+    $paletteLight = "palette-". $curPalette ."-light";
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         @include('includes.head')
     </head>
-    <body class="bodybackgroundcolor">
-        @include('includes.nav')
+    <body class="bodybackgroundcolor {{ $paletteMedium }}">
+        @include('includes.nav', ['paletteDark' => $paletteDark])
         
-        <div class="container body-content">
+        <div class="container body-content {{ $paletteLight }}">
             @include('includes.message')
             
             @yield('body')
